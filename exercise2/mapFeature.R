3c6da58bc4be5e48e15decf970173f8e16c212b4
Lemmawool <treyottaway@gmail.com>
2018-01-09 19:05:50 -0500

    Got mapFeature working

--- exercise2/mapFeature.R
+++ exercise2/mapFeature.R
@@ -1,7 +1,14 @@
 # Maps two features to a set of new features depending on degree.
 mapFeature <- function(featureA, featureB, degree) {
-  out <- length(featureA)
+  X_out <- matrix(c(1), nrow=length(featureA), ncol=28)
+  current <- 2
+  
   for (i in 1:degree) {
-    
+    for (j in 0:i) {
+      X_out[, current] <- (featureA ^ (i - j)) * (featureB ^ j)
+      current <- current + 1
+    }
   }
+  
+  return (X_out)
 }
--- exercise2/r_practice2_2.R
+++ exercise2/r_practice2_2.R
@@ -2,12 +2,13 @@
 
 source("sigmoid.R")
 source("costFuncReg.R")
+source("mapFeature.R")
 
 train <- read.csv('~/R-Practice/exercise2/exercise2data2.txt', header=FALSE)
 X <- as.matrix(train[,1:2])
 y <- as.matrix(train[,3])
-X
-y
+
+X <- mapFeature(X[,1], X[,2],6)
 
 
 
