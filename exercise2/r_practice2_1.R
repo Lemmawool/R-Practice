5e902e4771b17b2f6d4404cf95d8349ab028cabe
Lemmawool <treyottaway@gmail.com>
2018-01-18 18:46:26 -0500

    Corrected the error in the calling of the gradient function

--- exercise2/r_practice2_1.R
+++ exercise2/r_practice2_1.R
@@ -15,10 +15,10 @@ theta <- matrix(c(rep(0,n+1)), nrow = n+1, ncol = 1)
 theta2 <- matrix(c(-24, 0.2, 0.2), nrow = n+1, ncol = 1)
 
 cost <- costFunc(theta, X, y)
-grad <- gradientFunc(theta, X, y)
+grad <- gradFunc(theta, X, y)
 
 cost2 <- costFunc(theta2, X, y)
-grad2 <- gradientFunc(theta2, X, y)
+grad2 <- gradFunc(theta2, X, y)
 
 result <- optim(theta, costFunc, gradFunc, X=X, y=y, method="BFGS", control=list(maxit=500))
 theta <- result$par
